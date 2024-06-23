839c2fd674ed80453d19bc466b93c3dc953b3ced
ashtilawat23 <ashtilawat23@gmail.com>
2024-06-23 09:50:32 -0400

    ready

--- app/pr.py
+++ app/pr.py
@@ -1,11 +1,2 @@
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
-
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", "Output a confirmation message highlighting that the given code has been committed to Github and a pull request has been created."),
-        ("human", "{input}"),
-    ]
-)
-
-pr_chain = prompt | ChatOpenAI(model="gpt-4o", temperature= 0.7)
+def pr_chain():
+    return "The code has been successfully committed to GitHub, and a pull request has been created."
