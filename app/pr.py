66f9c1b8d017398e64d8cc358a4ad2e1aac3f98a
ashtilawat23 <ashtilawat23@gmail.com>
2024-06-23 10:22:10 -0400

    fixed pr

--- app/pr.py
+++ app/pr.py
@@ -1,2 +1,19 @@
-def pr_chain():
-    return "The code has been successfully committed to GitHub, and a pull request has been created."
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+SYSTEM_PROMPT="""
+
+This is a mock API call. 
+Return the following string no matter the input: 
+The code has been successfully committed to GitHub, and a pull request has been created.
+```
+"""
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", SYSTEM_PROMPT),
+        ("human", "{input}"),
+    ]
+)
+
+pr_chain = prompt | ChatOpenAI(model="gpt-4o", temperature= 0.8)
