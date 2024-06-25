8ddf29c9951e893aea3fba5da54cc4207b794932
ashtilawat23 <ashtilawat23@gmail.com>
2024-06-24 22:47:40 -0400

    added CORS

--- app/server.py
+++ app/server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
 from app.pr import pr_chain
@@ -8,11 +9,20 @@ from app.generate import generate_chain
 
 app = FastAPI()
 
+# CORS policy
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace with your actual frontend URL(s) in production
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 async def redirect_root_to_docs():
     return RedirectResponse("/docs")
 
-
+# Adding routes
 add_routes(app, pr_chain, enable_feedback_endpoint=True, path="/pr")
 add_routes(app, test_chain, enable_feedback_endpoint=True, path="/test")
 add_routes(app, review_chain, enable_feedback_endpoint=True, path="/review")
